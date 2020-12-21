cd9d47344d9c1361d417e00d44e6bf37a5ccca6c
Esmerald1no <brunocesmeraldo@hotmail.com.br>
2020-12-21 01:05:14 -0600

    Iterating through all subfolders and files

--- File_Renamer.py
+++ File_Renamer.py
@@ -5,8 +5,16 @@ os.chdir(r"C:\Users\Bruno\Dropbox\ImageProcessing_Bruno")
 
 directories = [name for name in os.listdir(os.getcwd()) if os.path.isdir(name)]
 
-def listSubDirectories (dirPath = os.getcwd()):
-    return
+def list_files(dir):
+    r = []
+    for root, dirs, files in os.walk(dir,topdown=True,followlinks=False):
+        print(dirs,files)
+        for name in files:
+            if name.endswith(".tif"):
+                print(name)
+                r.append(os.path.join(root, name))
+
+    return r
 
 
 print(directories)
@@ -17,9 +25,13 @@ if dirCount>1:
         print(f"{i+1}. {directories[i]}")
         currentDir = int(input())
         os.chdir(directories[currentDir-1])
+        currentDir = os.getcwd()
 else:   
     if (currentDir := input(f"The directory found is \"{directories[0]}\" is this the desired directory? [Y/N].\nIf this is not the desired directory, insert the path for the new directory or move this module to that location: \n")) != "Y":
         currentDir = currentDir.lstrip("N ")
         os.chdir(currentDir)
+    else:
+        os.chdir(directories[0])
+        currentDir = os.getcwd()
 
-print(os.getcwd())
+a = list_files(currentDir)
