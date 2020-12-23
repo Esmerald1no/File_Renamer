077c59a8fc9d697356fd1ee68e5b3048ceb3f660
Esmerald1no <brunocesmeraldo@hotmail.com.br>
2020-12-22 19:19:08 -0600

    Removed unnecessary functions and simplifyed code

--- File_Renamer.py
+++ File_Renamer.py
@@ -4,33 +4,22 @@ from collections import defaultdict
 #TEMP: Current Directory not the same as working directory.
 os.chdir(r"C:\Users\Bruno\Dropbox\ImageProcessing_Bruno")
 
-
-
-def list_files(dir):
-    r = []
-    fileCounter = 0
-    for root, dirs, files in os.walk(dir,topdown=True,followlinks=False):
-        #print(dirs,files)
-        for name in files:
-            if name.endswith(".tif"):
-                #print(name)
-                r.append(os.path.join(root, name))
-                filePathString = root + os.sep +name
-                if fileCounter == 0:
-                    infoTemplate = getInfoFromFolders(filePath=filePathString,mode=0)[1]
-                else:
-                    pass
-                fileCounter += 1 
-    return r
-
 def getDirs(path):
     directories = [name for name in os.listdir(currentPath) if os.path.isdir(name)]
+    if len(visitedDirs) > 0:
+        tempDirectories = [item for item in directories not in visitedDirs]
+        directories = tempDirectories
+
     dirCount = len(directories)
     if dirCount>1:
         print("Multiple Directories Found, Select Working Directory:")
         for i in range(dirCount):
             print(f"{i+1}. {directories[i]}")
             currentDir = int(input())
+
+            global visitedDirs
+            visitedDirs.append(directories[currentDir-1])
+
             os.chdir(directories[currentDir-1])
             currentDir = os.getcwd()
     else:   
@@ -43,21 +32,28 @@ def getDirs(path):
     return (directories,dirCount,currentDir)
 
 def getInfoFromFolders(filePath,separator = "_",infoTemplate=[],mode = 1):
-    #TODO: #3 Figure out how to get information out of "filestring".
     infoList = []
     if mode == 0:
+
         topDir = currentDir.split("\\")[-1]
         topDirPos = filePath.find(topDir)
+
         print(topDirPos)
+
         usefulInfoString = filePath[topDirPos:]
-        print(usefulInfoString)
-        tempList = usefulInfoString.split("\\")
-        fileName = tempList[-1]
-        print(fileName)
-        usefulInfoList = tempList[:-1]
+
         print(usefulInfoString)
 
+        tempList = usefulInfoString.split("\\")
+        fileName = tempList[-1]
+
+        print(fileName)
+
+        usefulInfoList = tempList[:-1]
+        
+        print(usefulInfoString)  
         #TODO: #5 Add logic for user to decide which naming pattern they want.
+
         infoTemplate = []
     else:
         infoTemplate = infoTemplate
@@ -74,14 +70,22 @@ def main():
     global directories,dirCount,currentDir
     directories,dirCount,currentDir = getDirs(currentPath)
 
-    #For Testing ONLY
-    a = list_files(currentDir)
-    with open(r"D:\Test.txt",'w') as f:
-        for item in a:
-            f.write(item+"\n")
+    fileCounter = 0
+    for root, dirs, files in os.walk(currentDir,topdown=True,followlinks=False):
+        for name in files:
+            if name.endswith(".tif"):
+                filePathString = root + os.sep +name
+                if fileCounter == 0:
+                    infoTemplate = getInfoFromFolders(filePath=filePathString,mode=0)[1]
+                else:
+                    pass
+                fileCounter += 1 
 
 directories,dirCount,currentDir,currentPath = [],0,"",""
-usefulInfoDict = defaultdict(dict)
+usefulInfoPosDict = defaultdict(dict)
+lenUsefulInfoPosDict = 0
+visitedDirs = []
+
 while 1:
     #TODO: #4 Define Looping condition.
     main()
