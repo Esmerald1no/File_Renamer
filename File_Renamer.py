d7e97a777b70079a790009d4ddacb206e80cdf1d
Esmerald1no <brunocesmeraldo@hotmail.com.br>
2020-12-23 02:09:49 -0600

    Function can now handle deeper folders

--- File_Renamer.py
+++ File_Renamer.py
@@ -40,57 +40,119 @@ def getDirs(path):
 
     return (directories,dirCount,currentDir)
 
-def getInfoFromFolders(filePath,separator = "_",infoTemplate=[],mode = 1):
+def getInfoFromFolders(filePath,separator = "_",infoTemplate=[],mode = "Retrieve Info",extraParams = None):
     infoList = []
+    separator = separator
+    infoTemplate = infoTemplate
 
     def splitString(string, separator = "_"):
 
         topDir = currentDir.split("\\")[-1]
         topDirPos = filePath.find(topDir)
 
-        #print(topDirPos)
-
         folderInfoString = filePath[topDirPos:]
-
-        #print(usefulInfoString)
 
         tempList = folderInfoString.split("\\")
         fileName = tempList[-1]
-
-        #print(fileName)
 
         folderInfoList = tempList[:-1]
 
         global currentDirectoryDepth
         if fileCounter == 0:
-         currentDirectoryDepth = len(folderInfoList)
+            currentDirectoryDepth = len(folderInfoList)
         
         newLength = len(folderInfoList)
 
-        if newLength > currentDirectoryDepth: 
-            print("A new tag was found")
-            
+        if newLength > currentDirectoryDepth:
+            print( 3*"\n" + f"{newLength-currentDirectoryDepth} new tag(s) were found!\n")
+            newUsefulInfo = folderInfoList[currentDirectoryDepth + 1 :-1]
 
-        splitString = folderInfoList.split(separator)
+            nonlocal infoTemplate
+            infoTemplate = getInfoFromFolders(filePath, infoTemplate=infoTemplate, mode="Make Template",extraParams=("Update Template",newUsefulInfo))[1]
+
+        if "Use Separator" in extraParams:
+            splitString = folderInfoList.split(separator)
+        
+        else:
+            splitString = folderInfoList
         
         print(folderInfoList)
         
         return splitString,fileName
 
-    if mode == 0:
+    if mode == "Make Template":
         #TODO: #5 Add logic for user to decide which naming pattern they want.
 
-        usefulInfoList = splitString(filePath)[0]
-
-        print(3*"\n"+"These are an example the following tags found in the folder names.\nYou will be asked to name them for conveniencce, then choose the tags wou wish to keep in the file name.\nIf any new tags are found you will be prompted if you wish to add them to the naming convention.\nWARNING: Consider what the tag represents rather than the actual tag when deciding the namimg convention. This prompt will show up only once.")
+        if "Update Template" in extraParams:
+            usefulInfoList = extraParams[1]
+        else: 
+            usefulInfoList = splitString(filePath)[0]
+            print(3*"\n"+"These are an example the following tags found in the folder names.\nYou will be asked to name them for conveniencce, then choose the tags wou wish to keep in the file name.\nIf any new tags are found you will be prompted if you wish to add them to the naming convention.\nWARNING: Consider what the tag represents rather than the actual tag when deciding the namimg convention. This prompt will show up only once.")
+            
         global usefulInfoPosDict
         for tag in usefulInfoList:
             tagName = input(f"What does the tag \"{tag}\" represent?\n")
             usefulInfoPosDict[tagName] = usefulInfoList.index(tag)
+        
+        if infoTemplate == []:
+            correctSelection = "N"
+            tempDict = {}
+            while correctSelection != "Y":
+                print(3*"\n"+"These are the tags you named earlier:")
+                for i, item in enumerate(usefulInfoPosDict.keys()):
+                    print(f"{i}. {item}")
+                    tempDict[i] = item
 
-        infoTemplate = []
-    else:
-        infoTemplate = infoTemplate
+                infoTemplateString = input("Using the numbers, choose which, and in what order the information should be coppied to the file name separated by spaces (unused numbers will be ignored):\n\n")
+
+                tempList2 = []
+                print("Your selection was:")
+                for i in infoTemplateString.split(" "):
+                    print(tempDict[i],end="_")
+                    tempList2.append(tempDict[i])
+                    
+                correctSelection = input("Is this correct?[Y/N]\n")
+
+            infoTemplate = tempList2
+
+            for i in tempDict.values() not in tempList2:
+                usefulInfoPosDict[i] = usefulInfoPosDict[i] + "(Unused)"
+            
+        else:
+            changePattern = ("New Tags were added since last selection, do you wish to update naming pattern? [Y/N]\n")
+            
+            if changePattern == "Y":
+                correctSelection = "N"
+                tempDict = {}
+                while correctSelection != "Y":
+                    print(3*"\n"+"This is the updated list of tags:")
+                    for i, item in enumerate(usefulInfoPosDict.keys()):
+                        print(f"{i}. {item}")
+                        tempDict[i] = item
+
+                    infoTemplateString = input("Using the numbers, choose which, and in what order the information should be coppied to the file name separated by spaces (unused numbers will be ignored):\n\n")
+
+                    tempList2 = []
+                    print("Your selection was:")
+                    for i in infoTemplateString.split(" "):
+                        print(tempDict[i],end="_")
+                        tempList2.append(tempDict[i])
+                        
+                    correctSelection = input("Is this correct?[Y/N]\n")
+
+                infoTemplate = tempList2
+
+                for i in tempDict.values() not in tempList2:
+                    usefulInfoPosDict[i] = usefulInfoPosDict[i] + "(Unused)"
+            else:
+                pass
+        
+        infoList = getInfoFromFolders(filePath, separator=separator, infoTemplate=infoTemplate, mode="Get Info")[0]
+
+    elif mode == "Get Info":
+        allInfoList = splitString(filePath)
+        #TODO: finish wtriting filering of the list
+        
     
     return (infoList, infoTemplate)
 
@@ -109,16 +171,18 @@ def main():
         for name in files:
             if name.endswith(".tif"):
                 filePathString = root + os.sep +name
+
+                global fileCounter,infoTemplate
                 if fileCounter == 0:
-                    infoTemplate = getInfoFromFolders(filePath=filePathString,mode=0)[1]
+                    infoTemplate = getInfoFromFolders(filePath=filePathString,mode="Make Template")[1]
                 else:
                     pass
 
-                global fileCounter
                 fileCounter += 1 
 
 exitFlag = False
 directories,visitedDirs,dirCount,currentDir,currentPath = [],[],0,"",""
+infoTemplate = []
 usefulInfoPosDict = defaultdict(dict)
 fileCounter,currentDirectoryDepth = 0,0
 
