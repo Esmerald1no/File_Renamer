bed3c04bffe6793604e4e2252afb499e0aee728d
Esmerald1no <brunocesmeraldo@hotmail.com.br>
2021-01-16 17:12:46 -0600

    Code cleanup, and Main Documentation

--- File_Renamer.py
+++ File_Renamer.py
@@ -1,25 +1,34 @@
+#Module importing:
+
 import os   
+
+#Function declaration:
 
 def getDirs(path):
     '''
-    short explanation
+    Finds the directories in the same folder and selects the current working directory from user input.
 
-    in depth explanation
+    Given the path of the current working directory, this function will seek all avaliable directories
+    in that path if they have not been visited yet. Then based on user selection, it will change the 
+    current working directory to match that selection.
 
     Parameters:
-        -
+        - path: (str) path that the module is in.
     
     Returns:
-        -
+        - dirCount: (int) How many directories are in the folder that the module is in (does not include visited directories).
+        - currentDir: (str) The string that contains the path to the current working directory. 
 
     '''
-    directories = [name for name in os.listdir(currentPath) if os.path.isdir(name)]
+    directories = [name for name in os.listdir(path) if os.path.isdir(name)] #Lists all directories in path
 
+    #Handles multiple runs of main(), updates the directories list if the program has already visited a given folder
     global visitedDirs
     if len(visitedDirs) > 0:
         tempDirectories = [item for item in directories not in visitedDirs]
         directories = tempDirectories
     
+    #This block serves as a control for the user to select the working directory depending on the number of directories
     dirCount = len(directories)
     if dirCount>1:
         print("Multiple directories found, Select Working Directory:")
@@ -48,24 +57,36 @@ def getDirs(path):
             visitedDirs.append(directories[0])
     else:
         print("No direcories left to rename.")
-        return ([],-1,"")
+        return (0,"")
 
-    return (directories,dirCount,currentDir)
+    return (dirCount,currentDir)
 
-def getInfoFromFolders(filePath,separator = "_",infoTemplate=[],mode = "Retrieve Info",extraParams = None,usefulInfoPosDict = {}):
+def getInfoFromFolders(filePath, separator = "_", infoTemplate=[], mode = "Retrieve Info", usefulInfoPosDict = {}, extraParams = None):
     '''
-    short explanation
+    Function responsble for creating the template for the naming convention, and retrieving the information from the folders.
 
-    in depth explanation
+    This function behaves differently depending on the mode paramete. If the mode is set to "Make template", it will take the
+    filePath, and call splitString() to get the "tags" used in renaming the files, it will then prompt the user to name them
+    and decide which ones will be used on the file template. If the mode is set to "Retrieve Info", given the infoTemplate it
+    will return the string that will be the name of the file by calling splitString() and joining the necessary tags.
 
     Parameters:
-        -
-    
+        - filePath: (str) Contains the path to the file to be renamed.
+        - separator: (str) Contains the separator that the user is using, if any.(Default: "_")
+        - infoTemplate: (list) Contains the names of the tags used in file renaming. Empty on the first calling, but populated afterward.(Default: [])
+        - mode: (str) Mode that the function will run as. (Default: "Retrieve Info")
+        - usefulInfoPosDict: (dict) Dictionary containing the position in the splited file path for the given tag.(Default = {})
+        - extraParams: (tuple) Additional parameters that the fucntion can use depending on the situation.(Default: None)
+
     Returns:
-        -
+        - infoList: (list) Filtered and ordered list of information used in renaming the file, based on filePath and infoTemplate.
+        - infoTemplate: (list) Contains the names of the tags used in file renaming. Empty on the first calling, but populated afterward.
+        - fileExtension: (str) File extension of the given file.
+        - usefulInfoPosDict: (dict) Dictionary containing the position in the splited file path for the given tag.
 
     '''
 
+    #Local Variable setup:
 
     infoList = []
     separator = separator
@@ -73,17 +94,26 @@ def getInfoFromFolders(filePath,separator = "_",infoTemplate=[],mode = "Retrieve
     fileExtension = ""
     usefulInfoPosDict = usefulInfoPosDict
 
+    #Nested Function Declaration:
+
     def splitString(string, separator = "_", extraParams = None):
         '''
-        short explanation
+        Given the file path, this function will return a list of information obtained from splitting the path using the sparator if provided.
 
-        in depth explanation
+        This function will take the file path from "string", and if extraParams is set to "Use Separator", it will take the separator from
+        "separator" if any is provided and return information obtained from splitting the file path using the system separator and the
+        separator. Otherwise, it will just split the file path using the system separator.
 
         Parameters:
-            -
+            - string: (str) String that is to be splitted by the function, usually a file path.
+            - separator: (str) Separator to be used in splitting the string.(Default: "_")
+            - extraParams: (str) Additional parameters that the fucntion can use depending on the situation.(Default: None)
         
         Returns:
-            -
+            - splitString: (list) List that contains all of the items obtained from spliting the file path with the system separator and separator.
+            - fileName: (str) Name of the current file, does not include the extension.
+            - fileExtension: (str) Extension of the current file.
+            - usefulInfoPosDict: (dict) Dictionary containing the position in the splited file path for the given tag.
 
         '''
         separator = separator
@@ -110,9 +140,9 @@ def getInfoFromFolders(filePath,separator = "_",infoTemplate=[],mode = "Retrieve
 
             nonlocal infoTemplate
             if separatorFlag:
-                _infoList, infoTemplate,_fileExtension, usefulInfoPosDict = getInfoFromFolders(filePath, separator=separator, infoTemplate=infoTemplate, mode="Make Template",extraParams=("Update Template",newUsefulInfo,"Use Separator"), usefulInfoPosDict= usefulInfoPosDict)
+                _infoList, infoTemplate,_fileExtension, usefulInfoPosDict = getInfoFromFolders(filePath, separator=separator, infoTemplate=infoTemplate, mode="Make Template", usefulInfoPosDict= usefulInfoPosDict, extraParams=("Update Template",newUsefulInfo,"Use Separator"))
             else:
-               _infoList, infoTemplate, _fileExtension, usefulInfoPosDict = getInfoFromFolders(filePath, separator=separator, infoTemplate=infoTemplate, mode="Make Template",extraParams=("Update Template",newUsefulInfo), usefulInfoPosDict= usefulInfoPosDict)
+               _infoList, infoTemplate, _fileExtension, usefulInfoPosDict = getInfoFromFolders(filePath, separator=separator, infoTemplate=infoTemplate, mode="Make Template", usefulInfoPosDict= usefulInfoPosDict, extraParams=("Update Template",newUsefulInfo))
 
             currentDirectoryDepth = newLength
 
@@ -132,7 +162,7 @@ def getInfoFromFolders(filePath,separator = "_",infoTemplate=[],mode = "Retrieve
         
         return (splitString,fileName,fileExtension,usefulInfoPosDict)   
 
-   
+    #Function Body:
     
     if mode == "Make Template":
         usefulInfoPosDict = {}
@@ -260,25 +290,23 @@ def renameFile(filePath,infoList=[],fileExtension="",separator = "_", failCounte
         failCounter +=1
         renameFile(filePath,infoList,fileExtension,separator,failCounter)
     
-def main():
+def main(usefulInfoPosDictList = {}):
     '''
-    short explanation
-
-    in depth explanation
+    Main function of the program.
 
     Parameters:
-        -
+        - usefulInfoPosDict: (dict) Dictionary containing the position in the splited file path for the given tag.(Default = {})
     
     Returns:
-        -
+        - exitFlag: (bool) Looping condition for the module. Always returns True.
+        - usefulInfoPosDict: (dict) Dictionary containing the position in the splited file path for the given tag.(Default = {})
 
     '''
     
-    global currentPath 
     currentPath = os.path.abspath(os.getcwd())
 
-    global directories,dirCount,currentDir
-    directories,dirCount,currentDir = getDirs(currentPath)
+    global dirCount,currentDir
+    dirCount,currentDir = getDirs(currentPath)
     if dirCount == -1: return False
     separator = "_"
     fileExtension = input("What is the file extension of the file(s) you are trying to rename?\n")
@@ -318,21 +346,26 @@ def main():
 
     return True, usefulInfoPosDict
 
+#Initial variable setup:
 
-exitFlag = False
-directories,visitedDirs,dirCount,currentDir,currentPath = [],[],0,"",""
-infoTemplate = []
-usefulInfoPosDict = {}
-fileCounter,currentDirectoryDepth = 0,0
-separatorFlag = False
+exitFlag = False #Looping Condition.
+visitedDirs = [] #List that contains the directories already visited by the module.
+dirCount = 0 #Number of directories that are in the same folder as the module(does not include visited directoreis).
+currentDir = "" #String that contains the path to the current working directory.
+infoTemplate = [] #List that contains the tags that should be present in the file name.
+usefulInfoPosDict = {} #Dictionary that contains the tag names and their position in the the splited file path.
+fileCounter,currentDirectoryDepth = 0,0 # Number of files already renamed, and the order of the deepest folder already visited respectively.
+separatorFlag = False #Flag that defines if the user is using a separator in their naming convention.
 
 
+#Main Loop:
 
 while not exitFlag:
-    exitFlag,usefulInfoPosDict = main()
+    exitFlag,usefulInfoPosDict = main(usefulInfoPosDict)
 
     if dirCount > 0:
-        if continueFlag := input(f"There are still {dirCount} directories remaining, do you wish to continue?[Y/N]:\n") not in ["Y","y"]:
+        if input(f"There are still {dirCount} directories remaining, do you wish to continue?[Y/N]:\n") not in ["Y","y"]:
             exitFlag = False
-        else: exitFlag = True
-    else: exitFlag = True
+            if input("Do you wish to use the same configurations as last time?[Y/N]:\n") not in ["Y","y"]:
+                global fileCounter
+                fileCounter = 0
