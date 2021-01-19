f9fa7fbdfe1635b762566fc75114bace221c0282
Esmerald1no <brunocesmeraldo@hotmail.com.br>
2021-01-18 18:37:54 -0600

    Documentation and storing information for later

--- File_Renamer.py
+++ File_Renamer.py
@@ -1,6 +1,6 @@
 #Module importing:
 
-import os   
+import os,pickle   
 
 #Function declaration:
 
@@ -170,38 +170,46 @@ def getInfoFromFolders(filePath, separator = "_", infoTemplate=[], mode = "Retri
         previousInfoList = []
         
         if extraParams != None and "Update Template" in extraParams: #Runs only if extraParams is set to "Update Template." Branch only runs if called from within splitString().
-            #Updates usefulInfoList
+            #Setup for updating the infolist.
             usefulInfoList = extraParams[1]
             previousInfoList = usefulInfoPosDict.keys()
         else:
             if separatorFlag:
+                #Retrieves usefulInfoList using separator
                 usefulInfoList, _fileName, _fileExtension, usefulInfoPosDict = splitString(filePath,separator,extraParams="Use Separator")
             else:
+                #Retrieves usefulInfoList without separator
                 usefulInfoList, _fileName, _fileExtension, usefulInfoPosDict = splitString(filePath)
             print(3*"\n"+"These are an example the following tags found in the folder names.\nYou will be asked to name them for convenience, then choose the tags wou wish to keep in the file name.\nIf any new tags are found you will be prompted if you wish to add them to the naming convention.\nWARNING: Consider what the tag represents rather than the actual tag when deciding the namimg convention. This prompt will show up only once.")
-            
+
+        #Questions the user about names for each tag and appends them to the usefulInfoPosDict as (Tag:index of tag).    
         for tag in usefulInfoList:
             if tag not in previousInfoList:
                 tagName = input(f"What does the tag \"{tag}\" represent?\n")
-                if tagName != "quit":
-                    usefulInfoPosDict[tagName] = usefulInfoList.index(tag)
-                else:
-                    return
+                usefulInfoPosDict[tagName] = usefulInfoList.index(tag)
         
-        if infoTemplate == []:
+        if infoTemplate == []:  #If it is the first time that the function is running, it will take down this path
             correctSelection = "N"
             tempDict = {}
+
+            #Reponsible for determining which tags will be used in the naming convention, converts user input to dictionary keys, 
+            #and updates the dictionary to keep track of used and unused keys.
             while correctSelection not in ["Y","y"]:
+                #Lists the named tags
                 print(3*"\n"+"These are the tags you named earlier:")
                 for i, item in enumerate(usefulInfoPosDict.keys()):
                     print(f"{i}. {item}")
                     tempDict[i] = item
 
+                #Asks the user which tags should be used in naming convention
                 infoTemplateString = input("Using the numbers, choose which,\nand in what order the information should be coppied to the file name separated by spaces (unused numbers will be ignored):\n")
                 
-                infoTemplateString = infoTemplateString.replace(".","").strip(" ")
+                infoTemplateString = infoTemplateString.replace(".","").strip(" ")  #Removes unecessary "fluff."
 
                 tempList2 = []
+                
+                #Displays the user selection converting from numbers to dictionary tags, and adds them 
+                #to a temporary list used to update the dictrionary.
                 print("Your selection was:")
                 for i in infoTemplateString.split(" "):
                     print(tempDict[int(i)],end="_")
@@ -211,9 +219,11 @@ def getInfoFromFolders(filePath, separator = "_", infoTemplate=[], mode = "Retri
 
             infoTemplate = tempList2
 
+            #Dictionary comprehension to update the dictionary.
             usefulInfoPosDict = {(value if value in tempList2 else value+" (Unused)"):key for (key,value) in tempDict.items()}
 
-        else:
+        else:   #Only runs if called from within splitString(), and is used to update the infoTemplate. Pretty much the same code as the normal function except for the dictionary comprehension.
+
             changePattern = input("New Tags were added since last selection, do you wish to update naming pattern? [Y/N]\n")
             
             if changePattern in ["Y","y"]:
@@ -237,61 +247,77 @@ def getInfoFromFolders(filePath, separator = "_", infoTemplate=[], mode = "Retri
 
                 infoTemplate = tempList2
 
+                #More complex dictionary comprehension.
                 for i in tempDict.values():
                     usefulInfoPosDictList = list(usefulInfoPosDict.keys())
-                    if i not in tempList2:
-                        if (i+" (Unused)") not in usefulInfoPosDictList:
+                    if i not in tempList2:  #If the new tag is unused, or if an old tag is not used anymore.
+                        if (i+" (Unused)") not in usefulInfoPosDictList:    #If the tag is not already marked as unused.
                             usefulInfoPosDict = { (value+" (Unused)" if value == i else value):key for (key,value) in tempDict.items() }
-                    elif i in tempList2:
-                        if  i in usefulInfoPosDictList and " (Unused)" in i:
+                    elif i in tempList2:    #If the new tag is used in the naming convention, or if a previously unused tag is now used.
+                        if  i in usefulInfoPosDictList and " (Unused)" in i: #If the tag is a previously unused tag.
                             iIndex = list(tempDict.keys())[list(tempDict.values()).index(i)]
                             d1 = {iIndex:i.replace(" (Unused)","")}
                             tempDict.update(d1)
+                            #Removes the "Unused" mark from the respective tag.
                             usefulInfoPosDict = { (i.replace(" (Unused)","") if value == i else value):key for (key,value) in tempDict.items() }
 
             else:
                 print("Ignoring new entries.")
 
+    #If the mode is set to "Get Info."
     elif mode == "Get Info":
-        if separatorFlag:
+        if separatorFlag:   #Get the information list using the separator
             allInfoList,_fileName,fileExtension, usefulInfoPosDict = splitString(filePath,separator,extraParams="Use Separator")
-        else:
+        
+        else:   #Get Information list without a separator
             allInfoList,_fileName,fileExtension, usefulInfoPosDict = splitString(filePath,separator)
-        for item in infoTemplate:
-            try:
+        
+        for item in infoTemplate:   #Preparing the return infolIST
+            try:    #Try to add the item to the list
                 infoPos = usefulInfoPosDict[item]
                 infoList.append(allInfoList[infoPos])
 
-            except IndexError:
+            except IndexError:  #If the item does not exist because it is trying to get information from a deeper folder than it is avalable.
                 infoList.append("")
 
     return (infoList, infoTemplate,fileExtension,usefulInfoPosDict)
 
-def renameFile(filePath,infoList=[],fileExtension="",separator = "_", failCounter = 0):
+def renameFile(filePath,infoList,fileExtension,separator = "_", failCounter = 0):
     '''
-    short explanation
+    Renames a file given a path and a list of information
 
-    in depth explanation
+    This function takes the file path of the file to be renamed, creates the new file name by joining the information from the infoList using
+    the separator. If it finds that a there is already a file with the same name it will append a number to the end, akin to a traditional 
+    file system.
 
     Parameters:
-        -
+        - filePath: (str) The string that contains the path to the file to be renamed.
+        - infoList: (list) List of information retrieved from the filePath that will be used to rename the file.
+        - fileExtension: (str) The extension of the file prior to be renamed, used in renaming.
+        - separator: (str) The separator to be used to join the the information in infoList.(Default: "_")
+        - failCounter: (int) The number of times that this function encountered an error, only used if there is already a file with the 
+        same name present on the folder.(Default: 0)
     
     Returns:
-        -
+        - None
 
     '''
 
     failCounter = failCounter
-    actualName = separator.join(infoList)
-    if failCounter == 0:
-        actualPath = filePath.split("\\")[:-1]
-        actualPath = (os.sep).join(actualPath) + os.sep + actualName + fileExtension
-    else:
+    actualName = separator.join(infoList) #Joins the information on infoList using the separator.
+
+    if failCounter == 0: #If the program has not encountered an error yet.
+
+        actualPath = filePath.split("\\")[:-1] # Retrieves the path for the file minus the previous name..
+        actualPath = (os.sep).join(actualPath) + os.sep + actualName + fileExtension #Creates the new file name.
+
+    else:   #If the program has failed before.
+        #It will add the number of fails into the file name, creating hopefully an unique file name.
         actualPath = (os.sep).join((filePath.split("\\")[:-1])) + os.sep + actualName + f"({str(failCounter)})" + fileExtension
 
-    try:
+    try:    #Try to rename the file.
         os.rename(filePath,actualPath)
-    except FileExistsError:
+    except FileExistsError: #If the file already exists.
         failCounter +=1
         renameFile(filePath,infoList,fileExtension,separator,failCounter)
     
@@ -308,47 +334,55 @@ def main(usefulInfoPosDictList = {}):
 
     '''
     
-    currentPath = os.path.abspath(os.getcwd())
+    currentPath = os.path.abspath(os.getcwd()) #Gets the current path that the module is working on.
 
     global dirCount,currentDir
-    dirCount,currentDir = getDirs(currentPath)
-    if dirCount == -1: return False
-    separator = "_"
-    fileExtension = input("What is the file extension of the file(s) you are trying to rename?\n")
+    dirCount,currentDir = getDirs(currentPath) #Calls getDirs().
 
-    if input(f"Please confirm that the desired file extension is ({fileExtension}) [Y/N]: ") not in ["Y","y"]:
-        fileExtension = input("What is the desired file extension?: ")
-    if "." not in fileExtension:
+    if dirCount == 0: return False #If there are no more directories to rename, exits the main function and breaks the while loop.
+
+    separator = "_"
+    fileExtension = input("What is the file extension of the file(s) you are trying to rename?\n")  #Asks the user for the file extension of the files they are trying to rename, used for locating the files in the next step.
+
+    if input(f"Please confirm that the desired file extension is ({fileExtension}) [Y/N]: ") not in ["Y","y"]:  #Confirms user selection.
+        fileExtension = input("What is the desired file extension?: ")  #Chance for the user to fix their mistake.
+    if "." not in fileExtension:    #If the user forgot the period in the file extension
         fileExtension = "."+fileExtension
     
-    
+    #Block responsible for managing separators within folder names. Asks the user if the user is using a separator and which,
+    #then sets the global flag for leting other functions know a separator is being used.
     separatorPrompt = input("Are you using separators to store information in the folder names?[Y/N]\nIf yes, please indicate it with a space in between: (Optional)") 
-    if separatorPrompt == "quit": return True  
+    if separatorPrompt == "quit": return True  #Exit option for the program
     if separatorPrompt != "N":
         separator = separatorPrompt.replace("Y ","").replace("y ","").replace("Y","").replace("Y","")
         global separatorFlag
         separatorFlag = True
         
 
-    for root, _dirs, files in os.walk(currentDir,topdown=True,followlinks=False):
-        for name in files:
-            if name.endswith(fileExtension):
-                filePathString = root + os.sep +name
+    for root, _dirs, files in os.walk(currentDir,topdown=True,followlinks=False):   #Iterates through every file in all folders and subfolders, ignores symbolic links
+        for name in files:  #For each file name.
+            if name.endswith(fileExtension):    #If the extension matches the user input.
+                filePathString = root + os.sep +name    #Generates the string containing the full path to the file.
+
                 global fileCounter,infoTemplate
-                if fileCounter == 0:                    
+                if fileCounter == 0:    #If it is the first file the function finds.
+                    #Creates the template list.
                     _infoList, infoTemplate, _fileExtension, usefulInfoPosDict = getInfoFromFolders(filePath=filePathString,separator=separator,mode="Make Template",usefulInfoPosDict= usefulInfoPosDict)
-                    if _fileExtension == 'quit': return True,{}
+                    #Obtains the information for that file using the template.
                     infoList,_infoTemplate,fileExtension, usefulInfoPosDict = getInfoFromFolders(filePathString, separator=separator, infoTemplate = infoTemplate, mode="Get Info", usefulInfoPosDict=usefulInfoPosDict)
+                    #Renames that file.
                     renameFile(filePathString,infoList,fileExtension,separator)
 
-                else:
+                else:   #If it is not the first file.
+                    #Get the folder information using the template.
                     infoList, _infoTemplate, fileExtension, usefulInfoPosDict= getInfoFromFolders(filePathString, separator=separator, infoTemplate = infoTemplate, mode="Get Info", usefulInfoPosDict= usefulInfoPosDict)
+                    #Rename the file.
                     renameFile(filePathString,infoList,fileExtension,separator)
 
 
-                fileCounter += 1
+                fileCounter += 1 #Increases file counter.
                 
-    dirCount -=1
+    dirCount -=1    #Decreases the number of directories remaning.
 
     return True, usefulInfoPosDict
 
@@ -374,4 +408,11 @@ while not exitFlag:
             exitFlag = False
             if input("Do you wish to use the same configurations as last time?[Y/N]:\n") not in ["Y","y"]:
                 global fileCounter
-                fileCounter = 0
+                fileCounter = 0 #If the file counter is 0, it will run through the setup steps once more.
+
+else:  
+    #Once the module finishes runing, it saves the information list and usefulInfoPosDict to a file, used with spheroid_detection.py to
+    #generate a report.
+    with open("reportInformation.pkl","wb") as f:   #Creates the binary dump.
+        pickle.dump(infoTemplate,f) #Wrtites infoTemplate to the dump file.
+        pickle.dump(usefulInfoPosDict,f) #Writes usefulInfoPosDict to the dump file.
